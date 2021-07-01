d712821bd6b34350ac5c44c2f75e7a3feed648d8
Jonathan Berrisch <jonathan@berrisch.biz>
2021-07-01 17:08:30 +0200

    Make online a generic function

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -80,59 +80,8 @@ mat2vec <- function(x) {
     .Call(`_profoc_mat2vec`, x)
 }
 
-#' @template function_online
-#'
-#' @template param_y
-#' @template param_experts
-#' @template param_tau
-#' @template param_intercept
-#' @template param_loss_function
-#' @template param_loss_parameter
-#' @template param_ex_post_smooth
-#' @template param_ex_post_fs
-#' @template param_smooth_lambda
-#' @template param_method
-#' @param method_var Allows to calculate slight variations of the BOA
-#' algorithm
-#' @param forget_regret Share of past regret not to be considered, resp. to be
-#' forgotten in every iteration of the algorithm. Defaults to 0.
-#' @template param_forget_performance
-#' @template param_fixed_share
-#' @param gamma Scaling parameter for the learning rate.
-#' @template param_smooth_ndiff
-#' @template param_smooth_deg
-#' @template param_basis_deg_online
-#' @template param_smooth_knot_distance
-#' @template param_basis_knot_distance_online
-#' @template param_smooth_knot_distance_power
-#' @template param_basis_knot_distance_power
-#' @param gradient Determines if a linearized version of the loss is used.
-#' @param loss_array User specified loss array. If specified, the loss will not be calculated by profoc.
-#' @param regret_array User specified regret array. If specifiec, the regret will not be calculated by profoc.
-#' @template param_trace
-#' @param init_weights Matrix of dimension Kx1 or KxP used as starting weights. Kx1 represents the constant solution with equal weights over all P whereas specifiying a KxP matrix allows different starting weights for each P.
-#' @template param_lead_time
-#' @template param_allow_quantile_crossing
-#' @template param_soft_threshold
-#' @template param_ex_post_soft_threshold
-#' @template param_hard_threshold
-#' @template param_ex_post_hard_threshold
-#' @template param_max_parameter_combinations
-#' @usage online(y, experts, tau, intercept = FALSE, loss_function = "quantile",
-#' loss_parameter = 1, ex_post_smooth = FALSE, ex_post_fs = FALSE,
-#' smooth_lambda = -Inf, method = "boa", method_var = "A", forget_regret = 0,
-#' forget_performance = 0, fixed_share = 0, gamma = 1, smooth_ndiff = 1, smooth_deg = 3,
-#' basis_deg = 3,
-#' smooth_knot_distance = c(2^seq(log(1/(length(tau)+1),2)-1, -1, length=5),1),
-#' basis_knot_distance = c(2^seq(log(1/(length(tau)+1),2)-1, -1, length=5),1),
-#' smooth_knot_distance_power = 1, basis_knot_distance_power = 1,
-#' gradient = TRUE, loss_array = NULL, regret_array = NULL,
-#' trace = TRUE, init_weights = NULL, lead_time = 0, allow_quantile_crossing = FALSE,
-#' soft_threshold = -Inf, ex_post_soft_threshold = FALSE, hard_threshold = -Inf,
-#' ex_post_hard_threshold = FALSE, max_parameter_combinations = 100)
-#' @export
-online <- function(y, experts, tau = as.numeric( c()), intercept = FALSE, loss_function = "quantile", loss_parameter = 1, ex_post_smooth = FALSE, ex_post_fs = FALSE, smooth_lambda = as.numeric( c(-1 / 0)), method = "boa", method_var = "A", forget_regret = as.numeric( c(0)), forget_performance = 0, fixed_share = as.numeric( c(0)), gamma = as.numeric( c(1)), smooth_ndiff = as.numeric( c(1.5)), smooth_deg = as.numeric( c()), basis_deg = as.numeric( c(3)), smooth_knot_distance = as.numeric( c()), basis_knot_distance = as.numeric( c()), smooth_knot_distance_power = as.numeric( c()), basis_knot_distance_power = as.numeric( c(1)), gradient = TRUE, loss_array = as.numeric( c()), regret_array = as.numeric( c()), trace = TRUE, init_weights = NULL, lead_time = 0L, allow_quantile_crossing = FALSE, soft_threshold = as.numeric( c(-1 / 0)), ex_post_soft_threshold = FALSE, hard_threshold = as.numeric( c(-1 / 0)), ex_post_hard_threshold = FALSE, max_parameter_combinations = 100L) {
-    .Call(`_profoc_online`, y, experts, tau, intercept, loss_function, loss_parameter, ex_post_smooth, ex_post_fs, smooth_lambda, method, method_var, forget_regret, forget_performance, fixed_share, gamma, smooth_ndiff, smooth_deg, basis_deg, smooth_knot_distance, basis_knot_distance, smooth_knot_distance_power, basis_knot_distance_power, gradient, loss_array, regret_array, trace, init_weights, lead_time, allow_quantile_crossing, soft_threshold, ex_post_soft_threshold, hard_threshold, ex_post_hard_threshold, max_parameter_combinations)
+online_default <- function(y, experts, tau = as.numeric( c()), intercept = FALSE, loss_function = "quantile", loss_parameter = 1, ex_post_smooth = FALSE, ex_post_fs = FALSE, smooth_lambda = as.numeric( c(-1 / 0)), method = "boa", method_var = "A", forget_regret = as.numeric( c(0)), forget_performance = 0, fixed_share = as.numeric( c(0)), gamma = as.numeric( c(1)), smooth_ndiff = as.numeric( c(1.5)), smooth_deg = as.numeric( c()), basis_deg = as.numeric( c(3)), smooth_knot_distance = as.numeric( c()), basis_knot_distance = as.numeric( c()), smooth_knot_distance_power = as.numeric( c()), basis_knot_distance_power = as.numeric( c(1)), gradient = TRUE, loss_array = as.numeric( c()), regret_array = as.numeric( c()), trace = TRUE, init_weights = NULL, lead_time = 0L, allow_quantile_crossing = FALSE, soft_threshold = as.numeric( c(-1 / 0)), ex_post_soft_threshold = FALSE, hard_threshold = as.numeric( c(-1 / 0)), ex_post_hard_threshold = FALSE, max_parameter_combinations = 100L) {
+    .Call(`_profoc_online_default`, y, experts, tau, intercept, loss_function, loss_parameter, ex_post_smooth, ex_post_fs, smooth_lambda, method, method_var, forget_regret, forget_performance, fixed_share, gamma, smooth_ndiff, smooth_deg, basis_deg, smooth_knot_distance, basis_knot_distance, smooth_knot_distance_power, basis_knot_distance_power, gradient, loss_array, regret_array, trace, init_weights, lead_time, allow_quantile_crossing, soft_threshold, ex_post_soft_threshold, hard_threshold, ex_post_hard_threshold, max_parameter_combinations)
 }
 
 optimize_weights <- function(truth, experts, affine = FALSE, positive = FALSE, intercept = FALSE, debias = TRUE, loss_function = "quantile", tau = 0.5, forget = 0, loss_scaling = 1) {
--- src/online.cpp
+++ src/online.cpp
@@ -10,59 +10,8 @@
 
 using namespace arma;
 
-//' @template function_online
-//'
-//' @template param_y
-//' @template param_experts
-//' @template param_tau
-//' @template param_intercept
-//' @template param_loss_function
-//' @template param_loss_parameter
-//' @template param_ex_post_smooth
-//' @template param_ex_post_fs
-//' @template param_smooth_lambda
-//' @template param_method
-//' @param method_var Allows to calculate slight variations of the BOA
-//' algorithm
-//' @param forget_regret Share of past regret not to be considered, resp. to be
-//' forgotten in every iteration of the algorithm. Defaults to 0.
-//' @template param_forget_performance
-//' @template param_fixed_share
-//' @param gamma Scaling parameter for the learning rate.
-//' @template param_smooth_ndiff
-//' @template param_smooth_deg
-//' @template param_basis_deg_online
-//' @template param_smooth_knot_distance
-//' @template param_basis_knot_distance_online
-//' @template param_smooth_knot_distance_power
-//' @template param_basis_knot_distance_power
-//' @param gradient Determines if a linearized version of the loss is used.
-//' @param loss_array User specified loss array. If specified, the loss will not be calculated by profoc.
-//' @param regret_array User specified regret array. If specifiec, the regret will not be calculated by profoc.
-//' @template param_trace
-//' @param init_weights Matrix of dimension Kx1 or KxP used as starting weights. Kx1 represents the constant solution with equal weights over all P whereas specifiying a KxP matrix allows different starting weights for each P.
-//' @template param_lead_time
-//' @template param_allow_quantile_crossing
-//' @template param_soft_threshold
-//' @template param_ex_post_soft_threshold
-//' @template param_hard_threshold
-//' @template param_ex_post_hard_threshold
-//' @template param_max_parameter_combinations
-//' @usage online(y, experts, tau, intercept = FALSE, loss_function = "quantile",
-//' loss_parameter = 1, ex_post_smooth = FALSE, ex_post_fs = FALSE,
-//' smooth_lambda = -Inf, method = "boa", method_var = "A", forget_regret = 0,
-//' forget_performance = 0, fixed_share = 0, gamma = 1, smooth_ndiff = 1, smooth_deg = 3,
-//' basis_deg = 3,
-//' smooth_knot_distance = c(2^seq(log(1/(length(tau)+1),2)-1, -1, length=5),1),
-//' basis_knot_distance = c(2^seq(log(1/(length(tau)+1),2)-1, -1, length=5),1),
-//' smooth_knot_distance_power = 1, basis_knot_distance_power = 1,
-//' gradient = TRUE, loss_array = NULL, regret_array = NULL,
-//' trace = TRUE, init_weights = NULL, lead_time = 0, allow_quantile_crossing = FALSE,
-//' soft_threshold = -Inf, ex_post_soft_threshold = FALSE, hard_threshold = -Inf,
-//' ex_post_hard_threshold = FALSE, max_parameter_combinations = 100)
-//' @export
 // [[Rcpp::export]]
-Rcpp::List online(
+Rcpp::List online_default(
     mat &y,
     cube &experts,
     Rcpp::NumericVector tau = Rcpp::NumericVector::create(),
